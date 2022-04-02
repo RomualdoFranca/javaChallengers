1857245246864acd32fd4a66067698182f48a1d4
RomualdoFranca <romualdoADS2022@gmail.com>
2022-04-02 15:35:23 -0300

    criação das variáveis

---
+++ distanceBetweenTwoPoints.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class distanceBetweenTwoPoints {
+    public static void main(String[] args) {
+        //  variables
+        double x1, y1;
+        double x2, y2;
+        double distance = Math.sqrt(Math.pow((x2 - x1), 2))
+    }
+}
