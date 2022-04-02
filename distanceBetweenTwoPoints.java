72b0286e8682847709ad9ee23a02cb3ba9162f1e
RomualdoFranca <romualdoADS2022@gmail.com>
2022-04-02 16:10:36 -0300

    criando a formula de distancia entre dois pontos

--- distanceBetweenTwoPoints.java
+++ distanceBetweenTwoPoints.java
@@ -1,26 +1,26 @@
 package com.company;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class distanceBetweenTwoPoints {
     public static void main(String[] args) {
-
+//        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         //  variables
         double x1, y1;
         double x2, y2;
-        int a;
-        System.out.println("Insira a coordenada x1 do p1");
-        x1 = sc.nextInt();
-        System.out.println("Insira a coordenada y1 do p1");
-        y1 = sc.nextInt();
-        System.out.println("Insira a coordenada x2 do p2");
-        x2 = sc.nextInt();
-        System.out.println("Insira a coordenada y2 do p2");
-        y2 = sc.nextInt();
 
-//        a = Math.pow(a , 2);
-//        double distance = Math.sqrt(Math.pow((x2 - x1), 2))
+        System.out.println("Insira a coordenada x1 do p1");
+        x1 = sc.nextDouble();
+        System.out.println("Insira a coordenada y1 do p1");
+        y1 = sc.nextDouble();
+        System.out.println("Insira a coordenada x2 do p2");
+        x2 = sc.nextDouble();
+        System.out.println("Insira a coordenada y2 do p2");
+        y2 = sc.nextDouble();
+        double distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.printf("The distance is: %.4f\n", distance);
 
         sc.close();
     }
